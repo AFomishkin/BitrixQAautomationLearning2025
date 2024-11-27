d39dc5fe2aafcad31c699e497ca63ac49c199b4a
Андрей Фомишкин (Fourbear's) <fomishkin@bitrix.ru>
2024-11-27 15:55:44 +0200

    PortalLoginPageLogin.Login() local environment handling

--- ATlearning/ATframework3demo/PageObjects/PortalLoginPage.cs
+++ ATlearning/ATframework3demo/PageObjects/PortalLoginPage.cs
@@ -19,10 +19,11 @@ namespace atFrameWork2.PageObjects
         public PortalHomePage Login(User admin)
         {
             WebDriverActions.OpenUri(portalInfo.PortalUri);
-            var loginField = new WebItem("//input[@id='login']", "Поле для ввода логина");
-            var pwdField = new WebItem("//input[@id='password']", "Поле для ввода пароля");
+            var loginField = new WebItem("//input[@id='login' or @name='USER_LOGIN']", "Поле для ввода логина");
+            var pwdField = new WebItem("//input[@id='password' or @name='USER_PASSWORD']", "Поле для ввода пароля");
             loginField.SendKeys(admin.Login);
-            loginField.SendKeys(Keys.Enter);
+            if(!pwdField.WaitElementDisplayed(1))
+                loginField.SendKeys(Keys.Enter);
             pwdField.SendKeys(admin.Password, logInputtedText: false);
             pwdField.SendKeys(Keys.Enter);
             return new PortalHomePage();
