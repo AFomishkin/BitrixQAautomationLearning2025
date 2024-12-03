42afa26ede8f0ac667c450021ee67b12b76c877d
Андрей Фомишкин (Fourbear's) <fomishkin@bitrix.ru>
2024-12-03 12:28:56 +0200

    User, HelperMethods classes extended; EmployeeTools, Bitrix24Group classes added;

--- ATlearning/ATframework3demo/TestEntities/User.cs
+++ ATlearning/ATframework3demo/TestEntities/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ATframework3demo.BaseFramework.BitrixCPinterraction;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,7 +7,26 @@ namespace atFrameWork2.TestEntities
 {
     public class User
     {
+        public User() { }
+
+        public User(string loginAkaEmail, string password, string name, string lastName)
+        {
+            LoginAkaEmail = loginAkaEmail;
+            Password = password;
+            Name = name;
+            LastName = lastName;
+        }
+
         public string LoginAkaEmail { get; set; }
         public string Password { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string NameLastName => Name + " " + LastName;
+
+        public string GetDBid(Uri portalUri, User adminUser)
+        {
+            var result = PortalDatabaseExecutor.ExecuteQuery("select ID from b_user where EMAIL = '" + LoginAkaEmail + "'", portalUri, adminUser);
+            return result.Count == 0 ? null : result[0].ID;
+        }
     }
 }
